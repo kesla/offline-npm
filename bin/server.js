4215a3bb6dbab586ff52257398f5681501136e1c
David Björklund <david.bjorklund@gmail.com>
2016-06-08 19:42:45 -0400

    configure skim-registry & tarball url

--- bin/server.js
+++ bin/server.js
@@ -4,5 +4,7 @@ var home = require('home');
 var start = require('../dist/index').default;
 var port = process.env.PORT || 8044;
 var dir = home.resolve(process.env.OFFLINE_NPM_DIR || '~/.offline-npm');
+var skimUrl = process.env.SKIM_REGISTRY_RUL || 'https://skimdb.npmjs.com/registry';
+var tarballUrl = process.env.TARBALL_URL || 'https://registry.npmjs.org';
 
-start({port: port, dir: dir});
+start({port: port, dir: dir, skimUrl: skimUrl, tarballUrl: tarballUrl});
