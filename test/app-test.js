53bf1690d4794393dc12e40522984483e8b429ad
David Björklund <david.bjorklund@gmail.com>
2016-06-11 20:51:44 +0200

    return 500 on not notFound-error, more tests

--- test/app-test.js
+++ test/app-test.js
@@ -1,21 +1,44 @@
-import _servertest from 'servertest';
+import __servertest from 'servertest';
 import test from 'tapava';
 import Promise from 'bluebird';
 import startApp from '../lib/app';
 import http from 'http';
+import {NotFoundError} from 'level-errors';
+import assign from 'object-assign';
 
-const servertest = Promise.promisify(_servertest);
+const _servertest = Promise.promisify(__servertest);
+const servertest = (app, path, opts) =>
+  _servertest(http.createServer(app.callback()), path, opts);
+const jsontest = (app, path, opts) =>
+  servertest(app, path, assign({encoding: 'json'}, opts));
 
-test('app, known package', function * (t) {
+test('/:package, known', function * (t) {
   const data = {foo: 'bar'};
   const getPackage = packageName => {
     t.is(packageName, 'foo');
     return Promise.resolve(data);
   };
   const app = startApp({getPackage});
-  const server = http.createServer(app.callback());
-  const {body, statusCode} = yield servertest(server, '/foo', {encoding: 'json'});
+  const {body, statusCode} = yield jsontest(app, '/foo');
 
   t.deepEqual(body, data);
   t.is(statusCode, 200);
+});
+
+test('/:package, unknown', function * (t) {
+  const getPackage = packageName => Promise.reject(new NotFoundError());
+  const app = startApp({getPackage});
+  const {body, statusCode} = yield jsontest(app, '/beep');
+
+  t.deepEqual(body, {});
+  t.is(statusCode, 404);
+});
+
+test('/:package error', function * (t) {
+  const getPackage = packageName => Promise.reject(new Error('Beep boop'));
+  const app = startApp({getPackage});
+  const {body, statusCode} = yield servertest(app, '/beep');
+
+  t.is(body.toString(), 'Beep boop');
+  t.is(statusCode, 500);
 });
