2f1e512a2769ffc0d95c3de71910112ead3a64de
David Björklund <david.bjorklund@gmail.com>
2016-06-12 19:46:27 +0200

    get new (e.g uninstalled & relevant) versions

---
+++ test/get-new-packages-test.js
@@ -0,0 +1,33 @@
+import test from 'tapava';
+import {dirSync as tmp} from 'tmp';
+import {sync as touch} from 'touch';
+import {join} from 'path';
+import setupGetNewPackages from '../lib/get-new-packages';
+import Promise from 'bluebird';
+
+test('getNewPackages', function * (t) {
+  const {name: dir} = tmp();
+  touch(join(dir, 'foo-bar-1.0.0.tgz'));
+  touch(join(dir, 'foo-bar-1.1.0.tgz'));
+  touch(join(dir, 'foo-baz-2.0.0.tgz'));
+  const packages = {
+    get: packageName => {
+      t.is(packageName, 'foo-bar');
+
+      return Promise.resolve({
+        versions: {
+          '1.0.0': {},
+          '1.1.0': {},
+          '1.1.1': {},
+          '1.2.0': {},
+          '2.0.0': {}
+        }
+      });
+    }
+  };
+  const getNewPackages = setupGetNewPackages({dir, packages});
+
+  const actual = (yield getNewPackages('foo-bar')).sort();
+  const expected = ['1.1.1', '1.2.0', '2.0.0'];
+  t.deepEqual(actual, expected);
+});
