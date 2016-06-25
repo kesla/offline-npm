dea4243185c37c5e2e6eb7d499bddfd4b9c4cebf
David Bjorklund <david.bjorklund@gmail.com>
2016-06-25 19:36:03 +0200

    support /:packageName/:rangeOrTag

--- test/app-test.js
+++ test/app-test.js
@@ -44,6 +44,125 @@ test('/:package error', function * (t) {
   t.is(statusCode, 500);
 });
 
+test('/:package/:dist-tag', function * (t) {
+  const getPackage = packageName => {
+    t.is(packageName, 'beep');
+
+    return Promise.resolve({
+      'dist-tags': {
+        latest: '1.0.0'
+      },
+      versions: {
+        '2.0.0-beta1': {
+          version: '2.0.0-beta1'
+        },
+        '1.0.0': {
+          version: '1.0.0'
+        },
+        '0.5.0': {
+          version: '0.5.0'
+        },
+        '0.5.1': {
+          version: '0.5.1'
+        }
+      }
+    });
+  };
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield jsontest(app, '/beep/latest');
+  t.deepEqual(body, {version: '1.0.0'});
+  t.is(statusCode, 200);
+});
+
+test('/:package/:version', function * (t) {
+  const getPackage = packageName => {
+    t.is(packageName, 'beep');
+
+    return Promise.resolve({
+      versions: {
+        '1.0.0': {
+          version: '1.0.0'
+        },
+        '0.5.0': {
+          version: '0.5.0'
+        },
+        '0.5.1': {
+          version: '0.5.1'
+        }
+      }
+    });
+  };
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield jsontest(app, '/beep/^0.5.0');
+  t.deepEqual(body, {version: '0.5.1'});
+  t.is(statusCode, 200);
+});
+
+test('/:package/:version, unknown version', function * (t) {
+  const getPackage = packageName => {
+    t.is(packageName, 'beep');
+
+    return Promise.resolve({
+      versions: {
+        '1.0.0': {
+          version: '1.0.0'
+        },
+        '0.5.0': {
+          version: '0.5.0'
+        },
+        '0.5.1': {
+          versions: '0.5.1'
+        }
+      }
+    });
+  };
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield jsontest(app, '/beep/^0.5.2');
+  t.deepEqual(body, {});
+  t.is(statusCode, 404);
+});
+
+test('/:package/:version, unknown tag', function * (t) {
+  const getPackage = packageName => {
+    t.is(packageName, 'beep');
+
+    return Promise.resolve({
+      versions: {
+        '1.0.0': {
+          version: '1.0.0'
+        },
+        '0.5.0': {
+          version: '0.5.0'
+        },
+        '0.5.1': {
+          versions: '0.5.1'
+        }
+      }
+    });
+  };
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield jsontest(app, '/beep/none-existing-tag');
+  t.deepEqual(body, {});
+  t.is(statusCode, 404);
+});
+test('/:package/:version, unknown package', function * (t) {
+  const getPackage = packageName => Promise.reject(new NotFoundError());
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield jsontest(app, '/beep/latest');
+
+  t.deepEqual(body, {});
+  t.is(statusCode, 404);
+});
+
+test('/:package/:version, error', function * (t) {
+  const getPackage = packageName => Promise.reject(new Error('beep boop'));
+  const app = startApp({getPackage, getTarball: noop});
+  const {body, statusCode} = yield servertest(app, '/beep/latest');
+
+  t.deepEqual(body.toString(), 'beep boop');
+  t.is(statusCode, 500);
+});
+
 test('/tarballs/:packageName/:version.tgz', function * (t) {
   const getTarball = ({packageName, version}) => {
     t.is(packageName, 'bar');
