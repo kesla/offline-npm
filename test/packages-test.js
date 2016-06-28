4ad3a858f0f98127d3e0284986f6d8f61c06ae03
David Bjorklund <david.bjorklund@gmail.com>
2016-06-28 10:42:57 +0200

    get package from skim-db if not exists on local db

--- test/packages-test.js
+++ test/packages-test.js
@@ -1,5 +1,7 @@
 import test from 'tapava';
 import setupPackages from '../lib/packages';
+import {NotFoundError} from 'level-errors';
+import http from 'http';
 
 test('packages.get()', function * (t) {
   let called = 0;
@@ -31,7 +33,7 @@ test('packages.get()', function * (t) {
       }
     }
   };
-  const packages = setupPackages({db, port: 1234});
+  const packages = setupPackages({db, port: 1234, skimUrl: 'http://irrelevant'});
   const actual = yield packages.get('foo');
   t.deepEqual(actual, expected);
   t.is(called, 1, 'get() is called once');
@@ -60,6 +62,7 @@ test('packages.put & packages.get', function * (t) {
       t.deepEqual(data, input);
       return Promise.resolve(null);
     }
+    // no get needed since we cache the package in memory
   };
   const expected = {
     name: 'foo',
@@ -71,9 +74,49 @@ test('packages.put & packages.get', function * (t) {
       }
     }
   };
-  const packages = setupPackages({db, port: 1234});
+  const packages = setupPackages({db, port: 1234, skimUrl: 'http://irrelevant'});
   yield packages.put(input);
   t.is(called, 1);
   const actual = yield packages.get('foo');
   t.deepEqual(actual, expected);
+});
+
+test('packages.get() package not in db', function * (t) {
+  let called = 0;
+  const registryData = {
+    name: 'foo',
+    versions: {
+      '1.2.3': {
+        dist: {
+          tarball: 'http://localhost:1234/tarballs/foo/1.2.3.tgz'
+        }
+      }
+    }
+  };
+  const db = {
+    get: packageName => Promise.reject(new NotFoundError()),
+    put: (packageName, data) => {
+      t.is(packageName, 'foo');
+      t.deepEqual(data, registryData);
+      called++;
+      return Promise.resolve(null);
+    }
+  };
+  const skimServer = yield new Promise(resolve => {
+    const onRequest = (req, res) => {
+      t.is(req.url, '/registry/foo');
+      res.end(JSON.stringify(registryData));
+    };
+
+    const server = http.createServer(onRequest).listen(0, () => resolve(server));
+  });
+
+  const skimUrl = `http://localhost:${skimServer.address().port}/registry`;
+
+  const packages = setupPackages({db, port: 1234, skimUrl});
+  const actual = yield packages.get('foo');
+  t.deepEqual(actual, registryData);
+  t.is(called, 1, 'set() is called');
+  yield packages.get('foo');
+  t.is(called, 1, 'data is cached and saved');
 });
