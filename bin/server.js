99a3952546ee3df37ebbfe2857de5a76c2b0507f
David Bjorklund <david.bjorklund@gmail.com>
2016-07-08 14:51:13 +0200

    write out error in initialization

--- bin/server.js
+++ bin/server.js
@@ -7,4 +7,8 @@ var dir = home.resolve(process.env.OFFLINE_NPM_DIR || '~/.offline-npm');
 var skimUrl = process.env.SKIM_RUL || 'https://skimdb.npmjs.com/registry';
 var registryUrl = process.env.REGISTRY_URL || 'https://registry.npmjs.org';
 
-start({port: port, dir: dir, skimUrl: skimUrl, registryUrl: registryUrl});
+start({port: port, dir: dir, skimUrl: skimUrl, registryUrl: registryUrl})
+  .catch(err => {
+    console.error(err.message);
+    console.error(err.stack);
+  });
