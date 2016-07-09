44aac81c852cb6dbf89aa0ed87dd418f19c6388a
David Bjorklund <david.bjorklund@gmail.com>
2016-07-09 22:35:54 +0200

    work with packages w weird tarball locations

--- test/get-tarball-test.js
+++ test/get-tarball-test.js
@@ -4,11 +4,12 @@ import {dirSync as tmp} from 'tmp';
 import fs from 'then-fs';
 import {join} from 'path';
 import setupHttpServer from './utils/http-server';
+import Promise from 'bluebird';
 
 test('getTarball() local file', function * (t) {
   const {name: dir} = tmp();
   yield fs.writeFile(join(dir, 'package-1.0.0.tgz'), 'foobar');
-  const getTarball = setupGetTarball({dir, registryUrl: '/does/not/matter'});
+  const getTarball = setupGetTarball({dir, packages: {}});
   const tarball = yield getTarball({packageName: 'package', version: '1.0.0'});
   const expected = 'foobar';
   const actual = tarball.toString();
@@ -17,12 +18,26 @@ test('getTarball() local file', function * (t) {
 
 test('getTarball() remote file', function * (t) {
   const {name: dir} = tmp();
-  const {shutdown, baseUrl: registryUrl} = yield setupHttpServer((req, res) => {
-    t.is(req.url, '/package/-/package-1.0.0.tgz');
+  const {shutdown, baseUrl: tarballUrl} = yield setupHttpServer((req, res) => {
+    t.is(req.url, '/custom/package/url.tgz');
     res.end('foobar');
   });
 
-  const getTarball = setupGetTarball({dir, registryUrl});
+  const packages = {
+    get: packageName => {
+      t.is(packageName, 'package');
+      return Promise.resolve({
+        versions: {
+          '1.0.0': {
+            dist: {
+              tarball: `${tarballUrl}/custom/package/url.tgz`
+            }
+          }
+        }
+      });
+    }
+  };
+  const getTarball = setupGetTarball({dir, packages});
   const tarball = yield getTarball({packageName: 'package', version: '1.0.0'});
   const expected = 'foobar';
   const actual = tarball.toString();
